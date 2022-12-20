19db97089da10cee5249398af632749eb0c8644d
willianac <willianac@live.com>
2022-12-20 18:14:18 -0300

    add navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,17 @@
+import { NavLink } from "react-router-dom";
 import "./Navigation.css"
 
 function Navigation() {
     return (
         <nav>
-            <h1>Pokedex</h1>
+            <div className="nav-logo">
+                <h1>Pokedex</h1>
+            </div>
+            <div className="nav-links">
+                <NavLink end to="/" className={({ isActive }) => isActive ? "active-nav-link" : 'nav-link'}>Jogos</NavLink>
+                <NavLink to="/test" className={({ isActive }) => isActive ? "active-nav-link" : 'nav-link'}>Pokedex</NavLink>
+                <NavLink to="/ops" className={({ isActive }) => isActive ? "active-nav-link" : 'nav-link'}>Videos</NavLink>
+            </div>
         </nav>
     )
 }
