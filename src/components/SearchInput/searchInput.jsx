fae256423f2ef7a8c764ba7480f238f3043b1f49
willianac <willianac@live.com>
2023-01-07 12:18:06 -0300

    add search bar

--- src/components/SearchInput/searchInput.jsx
+++ src/components/SearchInput/searchInput.jsx
@@ -1,10 +1,10 @@
 import "./searchInput.css"
 
 
-export default function SearchInput() {
+export default function SearchInput({ searchChange }) {
     return (
         <div className="search-bar">
-            <input type="search" className="search-field" placeholder="Search"/>
+            <input type="search" className="search-field" placeholder="Search" onChange={searchChange}/>
             <img src="/assets/search-symbol.png" alt="a search symbol"/>
         </div>
     )
